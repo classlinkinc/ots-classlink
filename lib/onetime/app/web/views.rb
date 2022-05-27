016af0fd8be239b8de8afe2dff78b9c066e4d691
Matt Ross <matthew.ross@classlink.com>
2022-05-27 13:41:10 -0400

    adjusting expiration options

--- lib/onetime/app/web/views.rb
+++ lib/onetime/app/web/views.rb
@@ -178,24 +178,24 @@ module Onetime
           self[:authenticated] ? (option_count)/2 : option_count-1
         end
         def expiration_options
-          if @expiration_options.nil?
-            selected = (!sess || !sess.authenticated?) ? 7.days : 7.days
-            disabled = (!sess || !sess.authenticated?) ? 7.days : plan.options[:ttl]
+          #if @expiration_options.nil?
+            #selected = (!sess || !sess.authenticated?) ? 7.days : 7.days
+            #disabled = (!sess || !sess.authenticated?) ? 7.days : plan.options[:ttl]
             @expiration_options = []
-            if self[:authenticated]
-              if plan.options[:ttl] > 30.days
-                @expiration_options.push *[
-                  { :value => 90.days, :name => "3 months"},
-                  { :value => 60.days, :name => "2 months"}
-                ]
-              end
-              if plan.options[:ttl] >= 30.days
-                @expiration_options << { :value => 30.days, :name => "30 days"}
-              end
-              if plan.options[:ttl] >= 14.days
-                @expiration_options << { :value => 14.days, :name => "14 days"}
-              end
-            end
+            #if self[:authenticated]
+             # if plan.options[:ttl] > 30.days
+              #  @expiration_options.push *[
+               #   { :value => 90.days, :name => "3 months"},
+               #   { :value => 60.days, :name => "2 months"}
+              #  ]
+             # end
+             # if plan.options[:ttl] >= 30.days
+             #   @expiration_options << { :value => 30.days, :name => "30 days"}
+             # end
+             # if plan.options[:ttl] >= 14.days
+              #  @expiration_options << { :value => 14.days, :name => "14 days"}
+              #end
+           # end
             @expiration_options.push *[
               { :value => 7.days, :name => "7 days", :default => true},
               { :value => 30.days, :name => "30 days"},
